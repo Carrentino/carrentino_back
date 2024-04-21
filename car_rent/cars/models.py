871ee6d8cb9ebd63a6ac5b0a3e4e1670c1ed0104
Михаил <mxoilslukin@gmail.com>
2024-04-21 14:26:48 +0500

    cars models

--- car_rent/cars/models.py
+++ car_rent/cars/models.py
@@ -1,6 +1,7 @@
 from core.models import BaseAbstractModel
 from django.contrib.auth import get_user_model
 from django.contrib.postgres.indexes import GinIndex
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -40,13 +41,52 @@ class CarModel(models.Model):
         DIESEL = 'DT', _('Дизельное топливо')
         ELECTRO = 'EL', _('Электричество')
 
+    class DRIVE_CHOICES(models.TextChoices):
+        '''Choices of drive'''
+        RWD = 'RWD', _('Задний привод')
+        FWD = 'FWD', _('Передний привод')
+        AWD = 'AWD', _('Полный привод')
+
+    class GEARBOX_CHOICES(models.TextChoices):
+        '''Choices of gearbox'''
+        MANUAL = 'MA', _('Механическая')
+        AUTOMATIC = 'AU', _('Автоматическая')
+        ROBOT = 'AR', _('Робот')
+        CVT = 'AC', _('Вариатор')
+
+    class BODY_TYPE_CHOCIES(models.TextChoices):
+        '''Choices of body type'''
+        SEDAN = 'SE', _('Седан')
+        LIFTBACK = 'LF', _('Лифтбек')
+        COUPE = 'CP', _('Купе')
+        HATCHBACK_3 = 'H3', _('Хэтчбек 3 дв.')
+        HATCHBACK_5 = 'H5', _('Хэтчбек 5 дв.')
+        STATION_WAGON = 'SW', _('Универсал')
+        SUV_3 = 'S3', _('Внедорожник 3 дв.')
+        SUV_5 = 'S5', _('Внедорожник 5 дв.')
+        MINIVAN = 'MV', _('Минивен')
+        PICKUP = 'PC', _('Пикап')
+        LIMOUSINE = 'LM', _('Лимузин')
+        VAN = 'VN', _('Фургон')
+        CABRIOLET = 'CB', _('Кабриолет')
+
     title = models.CharField(max_length=100, verbose_name='Название')
+    engine_capacity = models.DecimalField(decimal_places=1, max_digits=3, validators=[
+                                          MinValueValidator(0.1)], verbose_name='Объем двигателя')
+    drive = models.CharField(
+        max_length=3, choices=DRIVE_CHOICES, blank=False, verbose_name='Привод')
+    gearbox = models.CharField(
+        max_length=2, choices=GEARBOX_CHOICES, blank=False, verbose_name='Коробка передач')
+    body_type = models.CharField(
+        max_length=2, choices=BODY_TYPE_CHOCIES, blank=False, verbose_name='Тип кузова')
     type_of_fuel = models.CharField(
-        max_length=10, choices=TYPES_OF_FUEL, verbose_name='Тип топлива', null=True)
-    fuel_consumption = models.FloatField(verbose_name='Расход топлива')
-    hp = models.IntegerField(verbose_name='Мощность')
+        max_length=2, choices=TYPES_OF_FUEL, verbose_name='Тип топлива', blank=False)
+    fuel_consumption = models.DecimalField(decimal_places=1, max_digits=3, validators=[
+                                           MinValueValidator(0.1)], verbose_name='Расход топлива')
+    hp = models.IntegerField(
+        validators=[MinValueValidator(1)], verbose_name='Мощность')
     brand = models.ForeignKey(
-        Brand, on_delete=models.PROTECT, null=True, verbose_name='Марка')
+        Brand, on_delete=models.PROTECT, verbose_name='Марка')
 
     objects = OrderingManager()
 
@@ -64,12 +104,16 @@ class CarModel(models.Model):
 
 class Car(BaseAbstractModel):
     '''Model of User's Car'''
-    car = models.ForeignKey(CarModel, on_delete=models.PROTECT, null=True)
-    score = models.FloatField(default=5.0)
-    price = models.IntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    latitude = models.FloatField()
-    langitude = models.FloatField()
+    car = models.ForeignKey(
+        CarModel, on_delete=models.PROTECT, verbose_name='Модель')
+    color = models.CharField(max_length=25, verbose_name='Цвет')
+    score = models.DecimalField(default=5.0, decimal_places=2, max_digits=3, validators=[
+                                MinValueValidator(0.0), MaxValueValidator(5.01)], verbose_name='Рейтинг')
+    price = models.IntegerField(verbose_name='Цена')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Владелец')
+    latitude = models.FloatField(verbose_name='Широта')
+    langitude = models.FloatField(verbose_name='Долгота')
 
 
 class CarOptions(models.Model):
